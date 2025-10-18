f174cd079eb2b30bbbda3770f0bc4d75f970c4d1
Alex-Kravchenkoo <alex_krav4enko1994@mail.ru>
2025-10-19 00:41:33 +0300

    HW 11

---
+++ src/main/java/org/example/TestNG.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class TestNG {
+    @Test
+    public void TestNG(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(5));
+
+        driver.get("https://the-internet.herokuapp.com");
+
+        List <WebElement> Links = (List<WebElement>) driver.findElements(By.cssSelector("#content ul a"));
+
+        WebElement Link = Links.get(9);
+
+        Link.click();
+
+
+    }
+}
---
+++ src/test/java/MySeleniumTest.java
@@ -0,0 +1,48 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class MySeleniumTest {
+    @Test
+    public void firstSeleniumTest(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(5));
+
+        driver.get("https://litecart.stqa.ru/en/");
+
+        WebElement cartLink = driver.findElement(By.id("cart"));
+
+        cartLink.click();
+
+        WebElement backLink = driver.findElement(By.cssSelector("a[href='https://litecart.stqa.ru/en/']"));
+
+        backLink.click();
+
+        WebElement searchBox = driver.findElement(By.name("query"));
+
+        searchBox.sendKeys("Green Duck");
+
+        searchBox.sendKeys(Keys.ENTER);
+
+//        List<WebElement> rubberDuckImages = driver.findElements(By.className("image-wrapper"));
+//
+//        Assert.assertEquals(rubberDuckImages.size(), 6)
+
+
+        //cartLink.click();
+
+        //WebElement backLink = driver.findElement(By.cssSelector("a.link"));
+
+        //backLink.click();
+
+    }
+}
